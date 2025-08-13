b96dcb935831ac08485d3c41976013fc2b5c58e4
Pedro_Romanelo <pedro.romanelo@outlook.com>
2025-08-12 23:23:36 -0300

    feat(popup): adds popup, feedback, position, and trigger types

---
+++ types.ts
@@ -0,0 +1,7 @@
+export type PopopType = 'coupon' | 'call-toaction' | 'lead-capture' | 'feedback';  // Tipos de pop-up disponíveis
+
+export type FeedbackType = 'stars' | 'emoji' | 'thumbs';  // Tipos de feedback disponíveis
+
+export type PopupPosition = 'center' | 'bottom-right' | 'bottom-left' | 'top-banner' | 'bottom-banner';  // Posições de pop-up disponíveis
+
+export type TriggerType = 'time' | 'scroll' | 'exit-intent' | 'click' 
